8be6fa29c28d3a7e55c6b66024c9ff5f8094b766
architectpoint00-sudo <architectpoint00@gmail.com>
2026-08-20 23:42:24 +0300

    seo: remove retired FaqJsonLd from homepage (Google retired May 2026)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
   TRUST_BADGES,
 } from '@/lib/data'
 import Ticker from '@/components/Ticker'
-import { FaqJsonLd, PricingJsonLd } from '@/components/JsonLd'
+import { PricingJsonLd } from '@/components/JsonLd'
 import PricingCard from '@/components/PricingCard'
 import TestimonialCard from '@/components/TestimonialCard'
 import FaqAccordion from '@/components/FaqAccordion'
@@ -19,7 +19,6 @@ import FaqAccordion from '@/components/FaqAccordion'
 export default function HomePage() {
   return (
     <>
-      <FaqJsonLd items={HOMEPAGE_FAQ} />
       <PricingJsonLd packages={PRICING_PACKAGES} />
       <Ticker />
 
